be0be50ac20cc6e71216ee326ffbb57a0294154b
rlratzel <rlratzel@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-05-14 22:28:12 +0000

    * Copying new setuptools-based setup.py from enthought_split branch.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'enthought.help',
+    version = '1.1.0',
+    description  = 'Help framework for envisage applications',
+    author       = 'Enthought, Inc',
+    author_email = 'info@enthought.com',
+    url          = 'http://code.enthought.com/ets',
+    license      = 'BSD',
+    zip_safe     = False,
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = [
+        "enthought.logger",
+        "enthought.traits",
+    ],
+    extras_require = {
+        "EnvisagePlugin": ["enthought.envisage"],
+    },
+    namespace_packages = [
+        "enthought",
+    ],
+)
+
