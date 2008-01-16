f979e5e6d64178d9ba30da1c3ddf0916be605943
phil <phil@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-01-16 17:33:46 +0000

    Added support for modifying users. Added the Search button to the user views.

--- enthought/permissions/default/user_database.py
+++ enthought/permissions/default/user_database.py
@@ -14,11 +14,11 @@
 
 
 # Enthought library imports.
-from enthought.pyface.api import error
+from enthought.pyface.api import error, information
 from enthought.traits.api import Dict, HasTraits, implements, Instance, \
         Password, Unicode
 from enthought.traits.ui.api import Handler, Item, View
-from enthought.traits.ui.menu import OKCancelButtons
+from enthought.traits.ui.menu import Action, OKCancelButtons
 
 # Local imports.
 from i_user_database import IUserDatabase
@@ -45,8 +45,8 @@ class UserDb(HasTraits):
     users = Dict
 
 
-class _UserAccount(UserAccount):
-    """This represents a single account when edited."""
+class _ViewUserAccount(UserAccount):
+    """This represents a single account when in a view."""
 
     # The password
     password = Password
@@ -54,11 +54,16 @@ class _UserAccount(UserAccount):
     # The password confirmation.
     confirm_password = Password
 
+    # The user database.
+    user_db = Instance('UserDatabase')
+
 
 class _UserAccountHandler(Handler):
-    """The traits handler for the user account view that validates the data."""
+    """The base traits handler for user account views."""
 
-    MIN_PASSWORD_LEN = 6
+    ###########################################################################
+    # 'Handler' interface.
+    ###########################################################################
 
     def close(self, info, is_ok):
         """Reimplemented to validate the data."""
@@ -67,12 +72,79 @@ class _UserAccountHandler(Handler):
         if not is_ok:
             return True
 
-        # Validate the form.
-        return self.validate(info.ui.context['object'])
+        # Validate the form, only closing the dialog if the form is valid.
+        return self.validate(self._user_account(info))
+
+    ###########################################################################
+    # '_UserAccountHandler' interface.
+    ###########################################################################
 
     def validate(self, uac):
-        """Actually do the validation of the given object and return True if
-        there were no problems."""
+        """This must be reimplemented to validate the given object and return
+        True if there were no problems."""
+
+        raise NotImplementedError
+
+    @staticmethod
+    def error(msg):
+        """Display an error message to the user."""
+
+        error(None, msg)
+
+    @staticmethod
+    def inform(msg):
+        """Display an information message to the user."""
+
+        information(None, msg)
+
+    def search(self, vuac, name, users):
+        """This must be reimplemented to search the user database and update
+        the viewed object appropriately."""
+
+        raise NotImplementedError
+
+    ###########################################################################
+    # Trait handlers.
+    ###########################################################################
+
+    def _search_clicked(self, info):
+        """Invoked by the "Search" button."""
+
+        # Get the user name and a read-only copy of the dictionary of users.
+        vuac = self._user_account(info)
+        db = vuac.user_db.readonly_copy()
+
+        if db is not None:
+            name = vuac.name.strip()
+
+            if name:
+                self.search(vuac, vuac.name.strip(), db.users)
+            else:
+                self.error("Please give a user name to search for.")
+
+    ###########################################################################
+    # Private interface.
+    ###########################################################################
+
+    @staticmethod
+    def _user_account(info):
+        """Return the user account instance being handled."""
+
+        return info.ui.context['object']
+
+
+class _AddUserAccountHandler(_UserAccountHandler):
+    """The traits handler for the add user account view."""
+
+    ###########################################################################
+    # '_UserAccountHandler' interface.
+    ###########################################################################
+
+    def validate(self, uac):
+        """Validate the given object and return True if there were no problems.
+        """
+
+        MIN_PASSWORD_LEN = 6
 
         if not uac.name.strip():
             self.error("A user name must be given.")
@@ -86,25 +158,122 @@ class _UserAccountHandler(Handler):
             self.error("A password must be given.")
             return False
 
-        if len(uac.password) < self.MIN_PASSWORD_LEN:
-            self.error("The password must be at least %d characters long." % self.MIN_PASSWORD_LEN)
+        if len(uac.password) < MIN_PASSWORD_LEN:
+            self.error("The password must be at least %d characters long." % MIN_PASSWORD_LEN)
             return False
 
         return True
 
-    def error(self, msg):
-        """Display an error message to the user."""
+    def search(self, vuac, name, users):
+        """Search the user database to see if a user already exists."""
 
-        error(None, msg)
+        # See if there is a user with the name.
+        if users.has_key(name):
+            self.error("A user called \"%s\" already exists." % name)
+        else:
+            self.inform("A user called \"%s\" doesn't currently exist." % name)
 
 
-def _UserAccountView(title):
-    """Create a view to handle a user account."""
+class _ModifyUserAccountHandler(_AddUserAccountHandler):
+    """The traits handler for the modify user account view that validates the
+    data."""
 
-    return View(Item(name='name'), Item(name='description'),
-            Item(name='password'), Item(name='confirm_password'), title=title,
-            kind='modal', buttons=OKCancelButtons,
-            handler=_UserAccountHandler())
+    ###########################################################################
+    # '_UserAccountHandler' interface.
+    ###########################################################################
+
+    def search(self, vuac, name, users):
+        """Search the user database and update the viewed object appropriately.
+        """
+
+        # See if there is a user with the name.
+        try:
+            uac = users[name]
+        except KeyError:
+            # Find the first user that starts with the name.
+            for n, uac in users.items():
+                if n.startswith(name):
+                    break
+            else:
+                if name:
+                    self.error("There is no user whose name starts with \"%s\"." % name)
+                else:
+                    self.error("No users have been defined.")
+
+                return
+
+        # Update the viewed object.
+        vuac.name = uac.name
+        vuac.description = uac.description
+        vuac.password = vuac.confirm_password = uac.password
+
+
+class _UserAccountView(View):
+    """The base view for handling user accounts."""
+
+    #### 'View' interface #####################################################
+
+    kind = 'modal'
+
+    ###########################################################################
+    # Trait handlers.
+    ###########################################################################
+
+    def _buttons_default(self):
+        """Return the view's buttons."""
+
+        # Create an action that will search the database.
+        buttons = [Action(name="Search")]
+        buttons.extend(OKCancelButtons)
+
+        return buttons
+
+
+class _AddUserAccountView(_UserAccountView):
+    """A view to handle adding a user account."""
+
+    #### 'View' interface #####################################################
+
+    kind = 'modal'
+
+    title = "Add a user"
+
+    ###########################################################################
+    # 'object' interface.
+    ###########################################################################
+
+    def __init__(self, **traits):
+        """Initialise the object."""
+
+        super(_AddUserAccountView, self).__init__(Item(name='name'),
+                Item(name='description'), Item(name='password'),
+                Item(name='confirm_password'), **traits)
+
+    ###########################################################################
+    # Trait handlers.
+    ###########################################################################
+
+    def _handler_default(self):
+        """Return the view's handler."""
+
+        return _AddUserAccountHandler()
+
+
+class _ModifyUserAccountView(_AddUserAccountView):
+    """A view to handle modifying a user account."""
+
+    #### 'View' interface #####################################################
+
+    title = "Modify a user"
+
+    ###########################################################################
+    # Trait handlers.
+    ###########################################################################
+
+    def _handler_default(self):
+        """Return the view's handler."""
+
+        return _ModifyUserAccountHandler()
 
 
 class UserDatabase(HasTraits):
@@ -147,8 +316,8 @@ class UserDatabase(HasTraits):
     def add_user(self):
 
         # Get the data from the user.
-        vuac = _UserAccount()
-        view = _UserAccountView("Add a user")
+        vuac = _ViewUserAccount(user_db=self)
+        view = _AddUserAccountView()
 
         if vuac.edit_traits(view=view).result:
             uac = UserAccount(name=vuac.name.strip(),
@@ -172,18 +341,56 @@ class UserDatabase(HasTraits):
                 error(None, str(e))
 
     def modify_user(self):
-        """TODO"""
 
-        from enthought.pyface.api import information
+        # Get the data from the user.
+        vuac = _ViewUserAccount(user_db=self)
+        view = _ModifyUserAccountView()
 
-        information(None, "This will eventually implement a TraitsUI based GUI for modifying users.")
+        if vuac.edit_traits(view=view).result:
+            uac = UserAccount(name=vuac.name.strip(),
+                    description=vuac.description, password=vuac.password)
+
+            # Update the data in the database.
+            try:
+                self._db.lock()
+
+                try:
+                    data = self._db.read()
+
+                    if not data.users.has_key(uac.name):
+                        raise PersistentError("The user \"%s\" doesn't exist." % uac.name)
+
+                    data.users[uac.name] = uac
+                    self._db.write(data)
+                finally:
+                    self._db.unlock()
+            except PersistentError, e:
+                error(None, str(e))
 
     def delete_user(self):
         """TODO"""
 
-        from enthought.pyface.api import information
-
         information(None, "This will eventually implement a TraitsUI based GUI for deleting users.")
+
+    ###########################################################################
+    # 'UserDatabase' interface.
+    ###########################################################################
+
+    def readonly_copy(self):
+        """Return the current user database (which should not be modified)."""
+
+        try:
+            self._db.lock()
+
+            try:
+                data = self._db.read()
+            finally:
+                self._db.unlock()
+        except PersistentError, e:
+            error(None, str(e))
+            data = None
+
+        return data
 
     ###########################################################################
     # Trait handlers.
