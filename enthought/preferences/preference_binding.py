4c120f17596387002cd0a5e34e09900984400642
martin <martin@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-02-20 23:04:24 +0000

    - comment editing

--- enthought/preferences/preference_binding.py
+++ enthought/preferences/preference_binding.py
@@ -151,13 +151,18 @@ class PreferenceBinding(HasTraits):
 def bind_preference(obj, trait_name, preference_path, preferences=None):
     """ Create a new preference binding. """
 
-    # This may lokk a bit wierd, but we build up the dictionary of traits to
-    # set when we construct the binding object so that we only pass the
-    # 'preferences' argument if it is specified. If we passed it in as
-    # normal then the default value of None counts as 'setting' the trait which
-    # prevents the default initializer from running. If we try to set the
-    # trait after construction time, it is too late as the binding has already
-    # been initialized.
+    # This may seem a bit wierd, but we manually build up a dictionary of
+    # the traits that need to be set at the time the 'PreferenceBinding'
+    # instance is created.
+    #
+    # This is because we only want to set the 'preferences' trait iff one
+    # is explicitly specified. If we passed it in with the default argument
+    # value of 'None' then it counts as 'setting' the trait which prevents
+    # the binding instance from defaulting to the package-global preferences.
+    # Also, if we try to set the 'preferences' trait *after* construction time
+    # then it is too late as the binding initialization is done in the
+    # constructor (we could of course split that out, which may be the 'right'
+    # way to do it ;^).
     traits = {
         'obj'             : obj,
         'trait_name'      : trait_name,
