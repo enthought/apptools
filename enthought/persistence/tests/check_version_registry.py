93886feca793b323498a51229a8b2c9d89ad1c13
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-15 00:54:28 +0000

    Added unittest to run version registry, this is necessary since the test is not nosetest compatible.

---
+++ enthought/persistence/tests/check_version_registry.py
@@ -0,0 +1,114 @@
+"""Tests for the version registry.
+
+"""
+# Author: Prabhu Ramachandran <prabhu_r@users.sf.net>
+# Copyright (c) 2005, Enthought, Inc.
+# License: BSD Style.
+
+# Standard library imports.
+import unittest
+
+# Enthought library imports.
+from enthought.traits.api import HasTraits
+
+from enthought.persistence import version_registry, state_pickler
+
+
+class Classic:
+    __version__ = 0
+
+class New(object):
+    __version__ = 0
+
+class TraitClass(HasTraits):
+    __version__ = 0
+    
+
+class Test(New):
+    __version__ = 1
+    def __init__(self):
+        self.a = Classic()
+
+class Handler:
+    def __init__(self):
+        self.calls = []
+    def upgrade(self, state, version):
+        self.calls.append(('upgrade', state, version))
+    def upgrade1(self, state, version):
+        self.calls.append(('upgrade1', state, version))
+        
+
+class TestVersionRegistry(unittest.TestCase):
+    def test_get_version(self):
+        """Test the get_version function."""
+        c = Classic()
+        v = version_registry.get_version(c)
+        res = [(('Classic', '__main__'), 0)]
+        self.assertEqual(v, res)
+        state = state_pickler.get_state(c)
+        self.assertEqual(state.__metadata__['version'], res)
+
+        n = New()
+        v = version_registry.get_version(n)
+        res = [(('New', '__main__'), 0)]
+        self.assertEqual(v, res)
+        state = state_pickler.get_state(n)
+        self.assertEqual(state.__metadata__['version'], res)
+
+        t = TraitClass()
+        v = version_registry.get_version(t)
+        res = [(('HasTraits', 'enthought.traits.has_traits'), -1),
+               (('TraitClass', '__main__'), 0)]
+        self.assertEqual(v, res)
+        state = state_pickler.get_state(t)
+        self.assertEqual(state.__metadata__['version'], res)
+
+    def test_reload(self):
+        """Test if the registry is reload safe."""
+        # A dummy handler.
+        def h(x, y):
+            pass
+
+        registry = version_registry.registry
+        registry.register('A', '__main__', h)
+        self.assertEqual(registry.handlers.get(('A', '__main__')), h)
+        reload(version_registry)
+        registry = version_registry.registry
+        self.assertEqual(registry.handlers.get(('A', '__main__')), h)
+        del registry.handlers[('A', '__main__')]
+        self.assertEqual(registry.handlers.has_key(('A', '__main__')), False)
+
+    def test_update(self):
+        """Test if update method calls the handlers in order."""        
+        registry = version_registry.registry
+
+        # First an elementary test.
+        c = Classic()
+        state = state_pickler.get_state(c)
+        h = Handler()
+        registry.register('Classic', '__main__', h.upgrade)
+        c1 = state_pickler.create_instance(state)
+        state_pickler.set_state(c1, state)
+        self.assertEqual(h.calls, [('upgrade', state, 0)])
+        # Remove the handler.
+        registry.unregister('Classic', '__main__')
+
+        # Now check to see if this works for inheritance trees.
+        t = Test()
+        state = state_pickler.get_state(t)
+        h = Handler()
+        registry.register('Classic', '__main__', h.upgrade)
+        registry.register('New', '__main__', h.upgrade)
+        registry.register('Test', '__main__', h.upgrade1)
+        t1 = state_pickler.create_instance(state)
+        state_pickler.set_state(t1, state)
+        # This should call New handler, then the Test and then
+        # Classic.
+        self.assertEqual(h.calls, [('upgrade', state, 0),
+                                   ('upgrade1', state, 1),
+                                   ('upgrade', state.a, 0)])
+        
+    
+
+if __name__ == "__main__":
+    unittest.main()
--- enthought/persistence/tests/test_version_registry.py
+++ enthought/persistence/tests/test_version_registry.py
@@ -1,128 +1,40 @@
-"""Tests for the version registry.
-
-"""
-# Author: Prabhu Ramachandran <prabhu_r@users.sf.net>
-# Copyright (c) 2005, Enthought, Inc.
-# License: BSD Style.
-
-# Standard library imports.
+#------------------------------------------------------------------------------
+# Copyright (c) 2008, Enthought, Inc.
+# All rights reserved.
+# 
+# This software is provided without warranty under the terms of the BSD
+# license included in enthought/LICENSE.txt and may be redistributed only
+# under the conditions described in the aforementioned license.  The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+# Thanks for using Enthought open source!
+# 
+# Author: Ilan Schnell, Enthought, Inc.
+#
+# Description:
+#     Unittest to run another (unittest) script directly.
+#     In some cases this is necessary when things cannot be tested
+#     with nosetests itself.
+#------------------------------------------------------------------------------
+import os
+import subprocess
 import unittest
 
-# Enthought library imports.
-from enthought.traits.api import HasTraits
+# NOTE:
+#   `check_version_registry.py` can not be run by nosetests directly because of
+#   namespace issues.   Therefore the test in this file (which nosetests
+#   will execute) will spawn a python interpreter running the unittest.
+#   This test checks the return code of the process and will fail if
+#   the return code is non-zero.
 
-from enthought.persistence import version_registry, state_pickler
-
-
-class Classic:
-    __version__ = 0
-
-class New(object):
-    __version__ = 0
-
-class TraitClass(HasTraits):
-    __version__ = 0
+class RefreshTestCase(unittest.TestCase):
     
-
-class Test(New):
-    __version__ = 1
-    def __init__(self):
-        self.a = Classic()
-
-class Handler:
-    def __init__(self):
-        self.calls = []
-    def upgrade(self, state, version):
-        self.calls.append(('upgrade', state, version))
-    def upgrade1(self, state, version):
-        self.calls.append(('upgrade1', state, version))
+    def test_run(self):
+        
+        retcode = subprocess.call(['python', 'check_version_registry.py'],
+                                  cwd=os.path.dirname(__file__))
+        
+        self.assertEqual(retcode, 0)
         
 
-class TestVersionRegistry(unittest.TestCase):
-    def test_get_version(self):
-        """Test the get_version function."""
-        c = Classic()
-        v = version_registry.get_version(c)
-        res = [(('Classic', '__main__'), 0)]
-        self.assertEqual(v, res)
-        state = state_pickler.get_state(c)
-        self.assertEqual(state.__metadata__['version'], res)
-
-        n = New()
-        v = version_registry.get_version(n)
-        res = [(('New', '__main__'), 0)]
-        self.assertEqual(v, res)
-        state = state_pickler.get_state(n)
-        self.assertEqual(state.__metadata__['version'], res)
-
-        t = TraitClass()
-        v = version_registry.get_version(t)
-        res = [(('HasTraits', 'enthought.traits.has_traits'), -1),
-               (('TraitClass', '__main__'), 0)]
-        self.assertEqual(v, res)
-        state = state_pickler.get_state(t)
-        self.assertEqual(state.__metadata__['version'], res)
-
-    def test_reload(self):
-        """Test if the registry is reload safe."""
-        # A dummy handler.
-        def h(x, y):
-            pass
-
-        registry = version_registry.registry
-        registry.register('A', '__main__', h)
-        self.assertEqual(registry.handlers.get(('A', '__main__')), h)
-        reload(version_registry)
-        registry = version_registry.registry
-        self.assertEqual(registry.handlers.get(('A', '__main__')), h)
-        del registry.handlers[('A', '__main__')]
-        self.assertEqual(registry.handlers.has_key(('A', '__main__')), False)
-
-    def test_update(self):
-        """Test if update method calls the handlers in order."""        
-        registry = version_registry.registry
-
-        # First an elementary test.
-        c = Classic()
-        state = state_pickler.get_state(c)
-        h = Handler()
-        registry.register('Classic', '__main__', h.upgrade)
-        c1 = state_pickler.create_instance(state)
-        state_pickler.set_state(c1, state)
-        self.assertEqual(h.calls, [('upgrade', state, 0)])
-        # Remove the handler.
-        registry.unregister('Classic', '__main__')
-
-        # Now check to see if this works for inheritance trees.
-        t = Test()
-        state = state_pickler.get_state(t)
-        h = Handler()
-        registry.register('Classic', '__main__', h.upgrade)
-        registry.register('New', '__main__', h.upgrade)
-        registry.register('Test', '__main__', h.upgrade1)
-        t1 = state_pickler.create_instance(state)
-        state_pickler.set_state(t1, state)
-        # This should call New handler, then the Test and then
-        # Classic.
-        self.assertEqual(h.calls, [('upgrade', state, 0),
-                                   ('upgrade1', state, 1),
-                                   ('upgrade', state.a, 0)])
-        
-    
-
-def test_suite():
-    """Collects all the tests to be run."""
-    suites = []
-    suites.append(unittest.makeSuite(TestVersionRegistry, 'test_'))
-    total_suite = unittest.TestSuite(suites)
-    return total_suite
-
-def test(verbose=2):
-    """Useful when you need to run the tests interactively."""
-    all_tests = test_suite()
-    runner = unittest.TextTestRunner(verbosity=verbose)
-    result = runner.run(all_tests)
-    return result, runner
-
-if __name__ == "__main__":
+if __name__ == "__main__":          
     unittest.main()
