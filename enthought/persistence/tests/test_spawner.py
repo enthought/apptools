63f2be8600af5beaa5b5ca4f1162a0a968de04cb
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-22 15:01:21 +0000

    Added test_spawner to run two tests. -- reviewed by Judah

---
+++ enthought/persistence/tests/test_spawner.py
@@ -0,0 +1,73 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2008, Enthought, Inc.
+# All rights reserved.
+# 
+# This software is provided without warranty under the terms of the BSD
+# license included in enthought/LICENSE.txt and may be redistributed only
+# under the conditions described in the aforementioned license.  The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+# Thanks for using Enthought open source!
+# 
+# Author: Ilan Schnell, Enthought, Inc.
+#
+# Description:
+#     Unittest to run another script directly.  In some cases this is
+#     necessary when things cannot be tested with nosetests itself.
+#------------------------------------------------------------------------------
+import sys
+import os.path
+import subprocess
+import unittest
+import tempfile
+
+from enthought.util.resource import store_resource, get_path
+
+
+class Tests(unittest.TestCase):
+
+    def setUp(self):
+        OS_handle, fname = tempfile.mkstemp()
+        self.tmpname = fname
+
+    
+    def test_file_path(self):
+        """
+            Run 'check_file_path.py' as a spawned process and test
+            return value,
+            The python source is stored into a temporary test file before
+            being executed in a subprocess.
+        """
+        store_resource('AppTools',
+                       os.path.join('enthought', 'persistence','tests',
+                                    'check_file_path.py'),
+                       self.tmpname)
+        
+        retcode = subprocess.call([sys.executable, self.tmpname],
+                                  cwd=get_path(Tests))
+        
+        self.assertEqual(retcode, 0)
+
+
+    def test_version_registry(self):
+        """
+            Run 'check_version_registry.py' as a spawned process and test
+            return value,
+            The python source is stored into a temporary test file before
+            being executed in a subprocess.
+        """
+        store_resource('AppTools',
+                       os.path.join('enthought', 'persistence','tests',
+                                    'check_version_registry.py'),
+                       self.tmpname)
+        
+        retcode = subprocess.call([sys.executable, self.tmpname])
+        
+        self.assertEqual(retcode, 0)
+
+
+    def tearDown(self):
+        os.unlink(self.tmpname)  
+        
+
+if __name__ == "__main__":
+    unittest.main()
