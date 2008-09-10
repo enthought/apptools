26e8b010c7dc71d1b3fbb3110c9b890f477e8c4b
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-09-10 21:42:42 +0000

    * Updated to depend on new trunk version of Enable.

--- setup_data.py
+++ setup_data.py
@@ -16,7 +16,7 @@ def etsdep(p, min, max=None, literal=False):
 # Declare our ETS project dependencies.
 BLOCKCANVAS = etsdep('BlockCanvas', '3.0.1') # -- only used by enthought.template
 CHACO = etsdep('Chaco', '3.0.1')  # -- only used by enthought.template
-ENABLE = etsdep('Enable', '3.0.1')  # -- only used by enthought.template
+ENABLE = etsdep('Enable', '3.0.2')  # -- only used by enthought.template
 ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.1')
 ENVISAGECORE = etsdep('EnvisageCore', '3.0.1')  # -- mostly in enthought.help, enthought.naming use is in a try..except
 ENVISAGEPLUGINS = etsdep('EnvisagePlugins', '3.0.1')  # -- only used by enthought.help
