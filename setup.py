15ae7aaee56bac9fbc1ef0cad99516e630558ec4
GaelVaroquaux <GaelVaroquaux@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-07-08 01:44:22 +0000

    ENH: Capture any errors in setupdocs and fail gracefully during the build.

--- setup.py
+++ setup.py
@@ -40,12 +40,13 @@ commonly needed by many applications
 Prerequisites
 -------------
 If you want to build AppTools from source, you must first install 
-`setuptools <http://pypi.python.org/pypi/setuptools/0.6c8>`_.
+`setuptools <http://pypi.python.org/pypi/setuptools/0.6c8>`_ and
+`setupdocs  <http://pypi.python.org/pypi/SetupDocs>`_
 """
 
 
-import os
-import zipfile
+import traceback
+import sys
 
 from distutils import log
 from distutils.command.build import build as distbuild
@@ -64,17 +65,25 @@ INFO = setup_data['INFO']
 # Pull the description values for the setup keywords from our file docstring.
 DOCLINES = __doc__.split("\n")
 
-
 class MyDevelop(develop):
     def run(self):
         develop.run(self)
-        self.run_command('build_docs')
+        try:
+            self.run_command('build_docs')
+        except:
+            log.warn("Couldn't build documentation:\n%s" %
+                     traceback.format_exception(*sys.exc_info()))
 
 
 class MyBuild(distbuild):
     def run(self):
         distbuild.run(self)
-        self.run_command('build_docs')
+        try:
+            self.run_command('build_docs')
+        except:
+            log.warn("Couldn't build documentation:\n%s" %
+                     traceback.format_exception(*sys.exc_info()))
+
 
 
 # The actual setup call.
