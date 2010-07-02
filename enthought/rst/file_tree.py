50d0174f85ef36043435c82b2d78fe4dca24b355
klodo <klodo@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-07-02 12:57:05 +0000

    Fixed a Windows permission problem in file_tree.py

--- enthought/rst/file_tree.py
+++ enthought/rst/file_tree.py
@@ -12,13 +12,13 @@ class FileNode(HasTraits):
     def __init__(self, path, *args, **kw):
         self.path = path
         super(FileNode, self).__init__(*args, **kw)
-    
+
     @cached_property
     def _get_name(self):
         return os.path.split(self.path)[-1]
 
 class DirectoryNode(HasTraits):
-    
+
     filters = List(Str)
     path = Str()
     children = Property(List, depends_on='path')
@@ -27,10 +27,10 @@ class DirectoryNode(HasTraits):
     def __init__(self, path, *args, **kw):
         self.path = path
         super(DirectoryNode, self).__init__(*args, **kw)
-    
+
     def _default_filters(self):
         return ['*.*']
-    
+
     @cached_property
     def _get_name(self):
         return os.path.split(self.path)[-1]
@@ -41,28 +41,34 @@ class DirectoryNode(HasTraits):
         for filter in self.filters:
             filter_path = os.path.join(self.path, filter)
             file_paths += os.path.join(glob.glob(filter_path))
-        
+
         files = [FileNode(path) for path in file_paths]
         files.sort(key=lambda node: node.name.lower())
-        
+
         dirs = []
         # Pass filenames through str() to convert from unicode.
-        names = [str(f) for f in os.listdir(self.path)]
-        names.sort(key=str.lower)
-        for fn in names:
-            path = os.path.join(self.path, fn)
-            if os.path.isdir(path) and not fn.startswith('.'):
-                dirs.append(DirectoryNode(path, filters=self.filters))
-                
+        try:
+            names = [str(f) for f in os.listdir(self.path)]
+            names.sort(key=str.lower)
+            for fn in names:
+                path = os.path.join(self.path, fn)
+                if os.path.isdir(path) and not fn.startswith('.'):
+                    dirs.append(DirectoryNode(path, filters=self.filters))
+        except WindowsError as win_err:
+            # On Windows 7 (maybe on Vista/XP too, but not tested there) 
+            # calling os.listdir() on certain folders raises a 
+            # "WindowsError: [Error 5] Access is denied" exception
+            print win_err
+
         return dirs + files
-    
+
 class FileTree(HasTraits):
     root_path = Directory('.')
     filters = List()
     selected = Str()
-    
+
     _root = Instance(DirectoryNode)
-    
+
     def traits_view(self, parent=None):
         nodes = [TreeNode(node_for=[FileNode],
                           children='',
@@ -72,29 +78,29 @@ class FileTree(HasTraits):
                           children='children',
                           label='name',
                           view=View())]
-        
+
         return View(Item('root_path', show_label=False),
-                    Item('_root', 
+                    Item('_root',
                          editor=TreeEditor(nodes=nodes,
                                            on_dclick=self._on_dclick,
                                            on_click=self._on_click,
                                            editable=False),
                          show_label=False))
-        
+
     ###########################################################################
     # initialization methods
-    ###########################################################################        
+    ###########################################################################
     def _filters_default(self):
         return ['*.*']
-    
+
     def __root_default(self):
-        return DirectoryNode(os.path.abspath(self.root_path), 
+        return DirectoryNode(os.path.abspath(self.root_path),
                              filters=self.filters)
-    
+
     ###########################################################################
     # protected methods
     ###########################################################################
-            
+
     def _on_dclick(self, node):
         if isinstance(node, DirectoryNode):
             self.root_path = node.path
@@ -104,10 +110,10 @@ class FileTree(HasTraits):
     def _on_click(self, node):
         if isinstance(node, FileNode):
             self.selected = node.path
-            
+
     ###########################################################################
     # traits handlers methods
-    ###########################################################################        
+    ###########################################################################
     def _root_path_changed(self, new):
         if not self.traits_inited():
             return
