05a20fc8b9ad635ad6ce12099bf5b8946e93fe0d
Martin Chilvers <martin.chilvers@gmail.com>
2011-03-20 17:39:08 +0000

    removed ApplicationPreferrences from api.py

--- enthought/preferences/api.py
+++ enthought/preferences/api.py
@@ -5,6 +5,3 @@ from preferences import Preferences
 from preference_binding import PreferenceBinding, bind_preference
 from preferences_helper import PreferencesHelper
 from scoped_preferences import ScopedPreferences
-
-# Requires 'Preferences' and 'ScopedPreferences'.
-from application_preferences import ApplicationPreferences
