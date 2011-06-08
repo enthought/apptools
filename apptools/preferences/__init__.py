b17e674b566482ce09e0ba8214171f6d1d88cc06
Ilan Schnell <ischnell@enthought.com>
2011-06-07 22:08:18 -0500

    update and cleanup __init__.py files

--- apptools/preferences/__init__.py
+++ apptools/preferences/__init__.py
@@ -1,21 +1,5 @@
-#------------------------------------------------------------------------------
-# Copyright (c) 2007 by Enthought, Inc.
+# Copyright (c) 2007-2011 by Enthought, Inc.
 # All rights reserved.
-#------------------------------------------------------------------------------
 """ Manages application preferences.
     Part of the AppTools project of the Enthought Tool Suite
 """
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except:
-    pass
-
-
-
-# For py2app / py2exe support
-try:
-    import modulefinder
-    for p in __path__:
-        modulefinder.AddPackagePath(__name__, p)
-except:
-    pass
