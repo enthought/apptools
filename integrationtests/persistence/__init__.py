75ec4294b2570afc6cdec18ba2220f924034475e
Ilan Schnell <ischnell@enthought.com>
2011-06-08 23:06:36 -0500

    ENH: remove unused __init__.py files

--- integrationtests/persistence/__init__.py
+++
@@ -1,7 +0,0 @@
-#------------------------------------------------------------------------------
-# Copyright (c) 2008 by Enthought, Inc.
-# All rights reserved.
-#------------------------------------------------------------------------------
-""" Integration tests for persistence. Part of the AppTools project of the
-    Enthought Tool Suite.
-"""
