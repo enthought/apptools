03b485b7898d34a3d829f8fcba8ea0e399d1b4b7
Prabhu Ramachandran <prabhu@enthought.com>
2014-01-29 00:32:15 +0530

    TEST: Clean up the tests.
    The `test_spawner.py` and `test_version_registry.py` files were used to
    run other tests and were not needed at all.  The real errors with the
    tests should have been fixed.  These test files were actually causing
    errors.

--- apptools/persistence/tests/check_file_path.py
+++ apptools/persistence/tests/check_file_path.py
@@ -106,20 +106,5 @@ class TestFilePath(unittest.TestCase):
                          join(cwd, 'foo', 't.vtk'))
 
 
-
-def test_suite():
-    """Collects all the tests to be run."""
-    suites = []
-    suites.append(unittest.makeSuite(TestFilePath, 'test_'))
-    total_suite = unittest.TestSuite(suites)
-    return total_suite
-
-def test(verbose=2):
-    """Useful when you need to run the tests interactively."""
-    all_tests = test_suite()
-    runner = unittest.TextTestRunner(verbosity=verbose)
-    result = runner.run(all_tests)
-    return result, runner
-
 if __name__ == "__main__":
     unittest.main()
