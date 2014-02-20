b661e01b345d173e71c8a9d3f6b17abf588a51aa
Pietro Berkes <pberkes@enthought.com>
2014-02-20 11:56:22 +0000

    I forgot to update a test when the ListSelection class changed.
    This is embarassing.

--- apptools/selection/tests/test_list_selection.py
+++ apptools/selection/tests/test_list_selection.py
@@ -8,8 +8,8 @@ class TestListSelection(unittest.TestCase):
     def test_list_selection(self):
         all_items = ['a', 'b', 'c', 'd']
         selected = ['d', 'b']
-        list_selection = ListSelection(source_id='foo', selected=selected,
-                                       all_items=all_items)
+        list_selection = ListSelection.from_available_items(
+            source_id='foo', selected=selected, all_items=all_items)
 
         self.assertEqual(list_selection.items, selected)
         self.assertEqual(list_selection.indices, [3, 1])
