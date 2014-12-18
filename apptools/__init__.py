c29d1a653a56fb4e52cac537b0e922461f5abb24
John Wiggins <jwiggins@enthought.com>
2014-12-18 12:12:32 -0600

    Bump the version to 4.3.0 and add development versioning.

--- apptools/__init__.py
+++ apptools/__init__.py
@@ -1,7 +1,10 @@
 # Copyright (c) 2007-2014 by Enthought, Inc.
 # All rights reserved.
 
-__version__ = '4.2.1'
+try:
+    from ._version import full_version as __version__
+except ImportError:
+    __version__ = 'not-built'
 
 __requires__ = [
     'traitsui',
