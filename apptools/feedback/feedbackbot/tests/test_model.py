b34a76838b926b03d2c7f145f1d3bc8e3c083c9c
Siddhant Wahal <swahal@siddhant-mbp.local>
2019-06-27 14:53:18 -0500

    Add test to ensure files_upload is called correctly

--- apptools/feedback/feedbackbot/tests/test_model.py
+++ apptools/feedback/feedbackbot/tests/test_model.py
@@ -4,6 +4,8 @@ Tests for FeedbackMessage model.
 
 import numpy as np
 import unittest
+from unittest.mock import patch
+from PIL import Image
 
 from traits.api import TraitError
 from traits.testing.unittest_tools import UnittestTools
@@ -36,6 +38,68 @@ class TestFeedbackMessage(unittest.TestCase, UnittestTools):
             FeedbackMessage(img_data=np.empty((1, 1, 3)),
                             token='xoxb-123456',
                             channels='general')
+
+    def test_send(self):
+
+        img_data = np.array([[[1, 2, 3]]], dtype=np.uint8)
+
+        token = 'xoxb-123456'
+
+        channels = '#general'
+
+        msg = FeedbackMessage(img_data=img_data,
+                              token=token,
+                              channels=channels)
+
+        msg.name = 'Tom Riddle'
+        msg.organization = 'Death Eather, Inc'
+        msg.description = 'No one calls me Voldy.'
+
+        expected_msg = \
+            'Name: {}\nOrganization: {}\nDescription: {}'.format(
+                msg.name, msg.organization, msg.description)
+
+        files_upload_found = False
+
+        with patch('apptools.feedback.feedbackbot.model.slack'):
+
+            with patch('apptools.feedback.feedbackbot.model.slack.WebClient') \
+                    as mock_client:
+
+                msg.send()
+
+                for call_ in mock_client.mock_calls:
+                    # Loop over all calls made to mock_client, including nested
+                    # function calls.
+
+                    # Glean function name, provided positional and keyword
+                    # arguemts in call_
+                    name, args, kwargs = call_
+
+                    if name == '().files_upload':
+
+                        files_upload_found = True
+
+                        # The following lines ensure that <files_upload> is
+                        # called with the correct arguments.
+
+                        # There shouldn't be any positional arguments.
+                        self.assertTupleEqual((), args)
+
+                        # The following lines check keyword arguments were
+                        # passed correctly.
+                        np.testing.assert_almost_equal(
+                            img_data, np.array(Image.open(kwargs['file'])))
+
+                        self.assertEqual(channels, kwargs['channels'])
+
+                        self.assertEqual(
+                            expected_msg, kwargs['initial_comment'])
+
+                if not files_upload_found:
+
+                    self.fail(
+                        "Call to Slack API method <files_upload> not found.")
 
 
 if __name__ == '__main__':
