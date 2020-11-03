cada71761442dc699ce534b44cd0122fac0ee732
Kit Yan Choi <kchoi@enthought.com>
2020-11-03 15:49:22 +0000

    Move pandas dependency to the method that obviously require pandas

--- apptools/io/h5/tests/test_table_node.py
+++ apptools/io/h5/tests/test_table_node.py
@@ -1,33 +1,42 @@
 import unittest
 
-import numpy as np
-from numpy.testing import assert_allclose
-from pandas import DataFrame
+from apptools._testing.optional_dependencies import (
+    numpy as np,
+    pandas,
+    tables,
+    requires_numpy,
+    requires_pandas,
+    requires_tables,
+)
 
-from ..table_node import H5TableNode
-from .utils import temp_h5_file
+if np is not None and tables is not None:
+    from ..table_node import H5TableNode
+    from .utils import temp_h5_file
 
 
 NODE = '/table_node'
 
 
+@requires_numpy
+@requires_tables
 class TableNodeTestCase(unittest.TestCase):
+
     def test_basics(self):
         description = [('a', np.float64), ('b', np.float64)]
         with temp_h5_file() as h5:
             h5table = H5TableNode.add_to_h5file(h5, NODE, description)
             h5table.append({'a': [1, 2], 'b': [3, 4]})
 
-            assert_allclose(h5table['a'], [1, 2])
-            assert_allclose(h5table['b'], [3, 4])
+            np.testing.assert_allclose(h5table['a'], [1, 2])
+            np.testing.assert_allclose(h5table['b'], [3, 4])
 
         dtype_description = np.dtype([('c', 'f4'), ('d', 'f4')])
         with temp_h5_file() as h5:
             h5table = H5TableNode.add_to_h5file(h5, NODE, dtype_description)
             h5table.append({'c': [1.2, 3.4], 'd': [5.6, 7.8]})
 
-            assert_allclose(h5table['c'], [1.2, 3.4])
-            assert_allclose(h5table['d'], [5.6, 7.8])
+            np.testing.assert_allclose(h5table['c'], [1.2, 3.4])
+            np.testing.assert_allclose(h5table['d'], [5.6, 7.8])
 
             assert len(repr(h5table)) > 0
 
@@ -36,9 +45,9 @@ class TableNodeTestCase(unittest.TestCase):
         with temp_h5_file() as h5:
             h5table = H5TableNode.add_to_h5file(h5, NODE, description)
             h5table.append({'a': [1, 2], 'b': [3, 4]})
-            assert_allclose(h5table['a'], (1, 2))
-            assert_allclose(h5table[['b', 'a']], [(3, 1),
-                                                (4, 2)])
+            np.testing.assert_allclose(h5table['a'], (1, 2))
+            np.testing.assert_allclose(
+                h5table[['b', 'a']], [(3, 1), (4, 2)])
 
     def test_keys(self):
         description = [('hello', 'int'), ('world', 'int'), ('Qux1', 'bool')]
@@ -47,14 +56,15 @@ class TableNodeTestCase(unittest.TestCase):
             h5table = H5TableNode.add_to_h5file(h5, NODE, description)
             assert set(h5table.keys()) == keys
 
+    @requires_pandas
     def test_to_dataframe(self):
         description = [('a', np.float64)]
         with temp_h5_file() as h5:
             h5table = H5TableNode.add_to_h5file(h5, NODE, description)
             h5table.append({'a': [1, 2, 3]})
             df = h5table.to_dataframe()
-            assert isinstance(df, DataFrame)
-            assert_allclose(df['a'], h5table['a'])
+            assert isinstance(df, pandas.DataFrame)
+            np.testing.assert_allclose(df['a'], h5table['a'])
 
 
 if __name__ == '__main__':
