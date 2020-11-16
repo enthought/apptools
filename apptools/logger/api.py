5f0454d31e751499fe6473c1df9cc5953ea3318a
Aaron Ayres <aayres@enthought.com>
2020-11-16 12:11:47 -0600

    remove deprecated create_log_file_handler

--- apptools/logger/api.py
+++ apptools/logger/api.py
@@ -1,4 +1,4 @@
-from .logger import add_log_queue_handler, create_log_file_handler
+from .logger import add_log_queue_handler
 from .logger import FORMATTER, LEVEL, LogFileHandler
 from .log_point import log_point
 from .filtering_handler import FilteringHandler
