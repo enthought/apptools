19bcdf63b08ca7cfb96ab30507c6a0468ae0bd72
Aaron Ayres <aayres@enthought.com>
2020-11-25 12:55:55 -0700

    add copyright headers for apptools.scripting

--- apptools/scripting/recorder.py
+++ apptools/scripting/recorder.py
@@ -1,12 +1,18 @@
+# (C) Copyright 2005-2020 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
 """
 Code to support recording to a readable and executable Python script.
 
 FIXME:
     - Support for dictionaries?
 """
-# Author: Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
-# Copyright (c) 2008-2015, Enthought, Inc.
-# License: BSD Style.
 
 import builtins
 import warnings
--- apptools/scripting/util.py
+++ apptools/scripting/util.py
@@ -1,8 +1,14 @@
+# (C) Copyright 2005-2020 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
 """Simple utility functions provided by the scripting API.
 """
-# Author: Prabhu Ramachandran <prabhu [at] aero . iitb . ac . in>
-# Copyright (c) 2008,  Prabhu Ramachandran
-# License: BSD Style.
 
 from .recorder import Recorder
 from .recorder_with_ui import RecorderWithUI
