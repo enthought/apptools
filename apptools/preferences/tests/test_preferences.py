0ae4f52f19a8c0ca9d7926e17c7de949097f24b4
Robert T. McGibbon <rmcgibbo@gmail.com>
2021-03-26 22:24:05 -0400

    importlib_resources -> importlib.resources (#284)

--- apptools/preferences/tests/test_preferences.py
+++ apptools/preferences/tests/test_preferences.py
@@ -17,7 +17,10 @@ import unittest
 from os.path import join
 
 # Major package imports.
-from importlib_resources import files
+try:
+    from importlib.resources import files
+except ImportError:
+    from importlib_resources import files
 
 # Enthought library imports.
 from apptools.preferences.api import Preferences
